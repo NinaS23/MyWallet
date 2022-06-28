e75724d9833f24a11f25bdecc72d6fa27faa03e4
MariaDuda <mariabevenuto062@gmail.com>
2022-06-28 14:21:34 -0300

    format code

--- projeto13-mywallet-front/mywallet/src/routes/Cadastro/index.js
+++ projeto13-mywallet-front/mywallet/src/routes/Cadastro/index.js
@@ -1,19 +1,19 @@
 import { BodyCadastro , Input , Cadastrar} from "./style.js";
 import { Link } from "react-router-dom"
-export default function Cadastro(){
-    return(
+export default function Cadastro() {
+    return (
         <>
             <BodyCadastro>
                 <h1>MyWallet</h1>
                 <div>
-                <Input type="text" placeholder="Nome"  />
-                <Input type="text" placeholder="E-mail"  />
-                <Input type="text" placeholder="Senha"  />
-                <Input type="text" placeholder="Confirme a senha"  />
+                    <Input type="text" placeholder="Nome" />
+                    <Input type="text" placeholder="E-mail" />
+                    <Input type="text" placeholder="Senha" />
+                    <Input type="text" placeholder="Confirme a senha" />
                 </div>
                 <button><Cadastrar>Cadastrar</Cadastrar></button>
                 <Link to={"/"}>
-                <h4>Já tem uma conta? Entre agora!</h4>
+                    <h4>Já tem uma conta? Entre agora!</h4>
                 </Link>
             </BodyCadastro>
         </>
--- projeto13-mywallet-front/mywallet/src/routes/Login/index.js
+++ projeto13-mywallet-front/mywallet/src/routes/Login/index.js
@@ -1,4 +1,4 @@
-import { Body , Input , Entrar} from "./style.js";
+import { Body, Input, Entrar } from "./style.js";
 import { Link } from "react-router-dom"
 export default function Login() {
     return (
@@ -6,14 +6,14 @@ export default function Login() {
             <Body>
                 <h1>MyWallet</h1>
                 <div>
-                <Input type="text" placeholder="E-mail"  />
-                <Input type="text" placeholder="Senha"  />
+                    <Input type="text" placeholder="E-mail" />
+                    <Input type="text" placeholder="Senha" />
                 </div>
                 <Link to={"/registro"}>
-                <button><Entrar>Entrar</Entrar></button>
+                    <button><Entrar>Entrar</Entrar></button>
                 </Link>
                 <Link to={"/cadastro"}>
-                <h4>Primeira vez? Cadastre-se!</h4>
+                    <h4>Primeira vez? Cadastre-se!</h4>
                 </Link>
             </Body>
         </>
