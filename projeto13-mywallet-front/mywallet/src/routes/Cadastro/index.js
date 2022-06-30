e0dada9390e3cf42ac2df97ebd3735c6fb744542
MariaDuda <mariabevenuto062@gmail.com>
2022-06-30 10:53:18 -0300

    get input value

--- projeto13-mywallet-front/mywallet/src/routes/Cadastro/index.js
+++ projeto13-mywallet-front/mywallet/src/routes/Cadastro/index.js
@@ -1,15 +1,40 @@
 import { BodyCadastro , Input , Cadastrar} from "./style.js";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import { useState } from "react";
 export default function Cadastro() {
+    const [ userInfo , setUserInfo] = useState({
+        nome: "",
+        email: "",
+        senha: "",
+        confirmarSenha: ""
+    })
+  
     return (
         <>
             <BodyCadastro>
                 <h1>MyWallet</h1>
                 <div>
-                    <Input type="text" placeholder="Nome" />
-                    <Input type="text" placeholder="E-mail" />
-                    <Input type="text" placeholder="Senha" />
-                    <Input type="text" placeholder="Confirme a senha" />
+                    <Input type="text"
+                    placeholder="Nome"  
+                    value={userInfo.nome}
+                    onChange={e => { setUserInfo({ ...userInfo, nome: e.target.value }) }}
+                    
+                    />
+                    <Input type="text" 
+                    placeholder="E-mail"
+                    value={userInfo.email}
+                    onChange={e => { setUserInfo({ ...userInfo, email: e.target.value }) }}
+                    />
+                    <Input type="text"
+                     placeholder="Senha"
+                     value={userInfo.senha}
+                     onChange={e => { setUserInfo({ ...userInfo, senha: e.target.value }) }} 
+                     />
+                    <Input type="text"
+                     placeholder="Confirme a senha" 
+                     value={userInfo.confirmarSenha}
+                     onChange={e => { setUserInfo({ ...userInfo, confirmarSenha: e.target.value }) }}
+                     />
                 </div>
                 <button><Cadastrar>Cadastrar</Cadastrar></button>
                 <Link to={"/"}>
--- projeto13-mywallet-front/mywallet/src/routes/Login/index.js
+++ projeto13-mywallet-front/mywallet/src/routes/Login/index.js
@@ -1,13 +1,17 @@
 import { Body, Input, Entrar } from "./style.js";
 import { Link } from "react-router-dom"
+import { useState } from "react";
 export default function Login() {
+    const [ email , setEmail ] = useState("")
+    const [ senha , setSenha ] = useState("")
+    
     return (
         <>
             <Body>
                 <h1>MyWallet</h1>
                 <div>
-                    <Input type="text" placeholder="E-mail" />
-                    <Input type="text" placeholder="Senha" />
+                    <Input type="text" placeholder="E-mail" value={email} onChange={(e) => setEmail(e.target.value)}  />
+                    <Input type="text" placeholder="Senha" value={senha} onChange={(e) => setSenha(e.target.value)} />
                 </div>
                 <Link to={"/registro"}>
                     <button><Entrar>Entrar</Entrar></button>
