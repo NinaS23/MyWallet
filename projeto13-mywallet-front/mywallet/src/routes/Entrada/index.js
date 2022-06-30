da772dca47dc65a15f8088299605271134e98352
MariaDuda <mariabevenuto062@gmail.com>
2022-06-30 10:58:23 -0300

    get input value from saida and entrada

--- projeto13-mywallet-front/mywallet/src/routes/Entrada/index.js
+++ projeto13-mywallet-front/mywallet/src/routes/Entrada/index.js
@@ -1,13 +1,17 @@
+import { useState } from "react";
 import {   Div , TituloEntrada , BodyEntrada , Salvar} from "./style.js";
 
 export default function Entrada() {
+    const [ valor , setValor ] = useState("")
+    const [ description , setDescription ] = useState("")
+  
     return (
         <>
             <BodyEntrada>
                 <TituloEntrada>Nova entrada</TituloEntrada>
                 <Div>
-                    <input type="text" placeholder="Valor" />
-                    <input type="text" placeholder="Descrição" />
+                    <input type="text" placeholder="Valor"  value={valor} onChange={(e) => setValor(e.target.value)}  />
+                    <input type="text" placeholder="Descrição" value={description} onChange={(e) => setDescription(e.target.value)} />
                     <button><Salvar>Salvar entrada</Salvar></button>
                 </Div>
             </BodyEntrada>
--- projeto13-mywallet-front/mywallet/src/routes/Saida/index.js
+++ projeto13-mywallet-front/mywallet/src/routes/Saida/index.js
@@ -1,12 +1,16 @@
+import { useState } from "react";
 import {   Div , TituloSaida , BodySaida , Salvar} from "./style.js";
 export default function Saida(){
+    const [ valor , setValor ] = useState("")
+    const [ description , setDescription ] = useState("")
+
     return(
         <>
             <BodySaida>
-                <TituloSaida>Nova entrada</TituloSaida>
+                <TituloSaida>Nova Saída</TituloSaida>
                 <Div>
-                    <input type="text" placeholder="Valor" />
-                    <input type="text" placeholder="Descrição" />
+                    <input type="text" placeholder="Valor"  value={valor} onChange={(e) => setValor(e.target.value)}  />
+                    <input type="text" placeholder="Descrição"  value={description} onChange={(e) => setDescription(e.target.value)}/>
                     <button><Salvar>Salvar saída</Salvar></button>
                 </Div>
             </BodySaida>
